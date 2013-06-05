c9ec682549a8b3f91d131a8eaab8b610efd21218
Sean Fisher <srtfisher@gmail.com>
2013-06-04 20:33:49 -0400

    Schema

--- sites/all/modules/groupgrade/lib/Drupal/ClassLearning/Models/WorkflowTask.php
+++ sites/all/modules/groupgrade/lib/Drupal/ClassLearning/Models/WorkflowTask.php
@@ -4,6 +4,6 @@ use Illuminate\Database\Eloquent\Model as ModelBase,
   Drupal\ClassLearning\Exception as ModelException;
 
 class WorkflowTask extends ModelBase {
-  protected $table = 'workflow_task';
+  protected $table = 'task';
   protected $primaryKey = 'wt_id';
 }
