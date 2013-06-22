87446568b75bb17cd0054a40c37f4e3677d4f602
Sean Fisher <srtfisher@gmail.com>
2013-06-22 00:29:30 -0400

    Factory

---
+++ lib/Drupal/ClassLearning/Workflow/TaskFactory.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @file
+ * Task Factory to generate the tasks for a workflow
+ *
+ * @package groupgrade
+ */
+namespace Drupal\ClassLearning\Workflow;
+
+/**
+ * Task Factory
+ */
+class TaskFactory {
+  /**
+   * Constructor
+   */
+  public function __construct()
+  {
+    
+  }
+}
