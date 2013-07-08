c625e04b6c10abca403b4434f5047b5c86653d74
Sean Fisher <srtfisher@gmail.com>
2013-07-08 12:34:08 -0400

    Initial lurking

--- views/Assignments.php
+++ views/Assignments.php
@@ -57,7 +57,11 @@ function gg_view_assignment_listing($section_id, $asec_id)
   $rows = [];
 
   if (count($createProblems) > 0) : foreach ($createProblems as $t) :
-    $rows[] = [sprintf('<a href="%s">%s</a>', url('class/task/'.$t->task_id), $t->data['problem'])];
+    $rows[] = [sprintf(
+      '<a href="%s">%s</a>',
+      url('class/task/'.$t->task_id),
+      word_limiter($t->data['problem'], 10)
+    )];
   endforeach; endif;
 
   return theme('table', array(
