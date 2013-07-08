f654017d5bfcaa24d0109e2c03f749701b8fef9b
Sean Fisher <srtfisher@gmail.com>
2013-07-08 14:07:42 -0400

    Debug of cron

--- lib/Drupal/ClassLearning/Models/WorkflowTask.php
+++ lib/Drupal/ClassLearning/Models/WorkflowTask.php
@@ -283,6 +283,8 @@ class WorkflowTask extends ModelBase {
     // Lastly, the callback
     $callbackName = str_replace(' ', '_', $this->type);
     InternalCallback::$callbackName($this);
+
+    echo sprintf('Calling internal callback %d with data %d \n', $callbackName, print_r($this));
   }
 
   /**
