11f5ca29d7705b449e454b12ce6bd292ee370305
Sean Fisher <srtfisher@gmail.com>
2013-07-15 11:39:36 -0400

    Doc publish

--- build/doc-publish.sh
+++ build/doc-publish.sh
@@ -5,15 +5,17 @@ git pull;
 apigen -c build/apigen.neon
 
 # Move to a folder outside GIT
-if [-d ../groupgrade-docs]; then
-  rm -rf ../groupgrade-docs;
+if [ -d "../groupgrade-docs" ]; then
+  rmdir ../groupgrade-docs;
 fi
 
-mv build/docs/ ../groupgrade-docs
+mv build/docs ../groupgrade-docs
 
 git checkout gh-pages
 rm -rf *
 
 mv ../groupgrade-docs/* ./
-git -a -m "Importing documentation"
+rm -rf ../groupgrade-docs
+
+git commit-a -m "Importing documentation"
 git push origin gh-pages
