8326e122565654a004e4ad3593f367b42dedf4e9
Sean Fisher <srtfisher@gmail.com>
2013-07-15 11:34:49 -0400

    Doc publish start

--- build/doc-publish.sh
+++ build/doc-publish.sh
@@ -4,6 +4,10 @@
 apigen -c build/apigen.neon
 
 # Move to a folder outside GIT
+if [-d ../groupgrade-docs]; then
+  rm -rf ../groupgrade-docs;
+fi
+
 mv build/docs/ ../groupgrade-docs
 
 git checkout gh-pages
